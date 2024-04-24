2817a8470604f3ad6397e063b09494ec27f24698
Alex Darr <alexdarr@gmail.com>
2024-04-24 11:20:33 -0400

    Ignore rate limit requests

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -453,13 +453,13 @@ command
 
       const octokit = createOctokit(accessToken, baseUrl, proxyUrl, logger);
 
-      const shouldCheckRateLimitAgain = await logRateLimitInformation(logger, octokit);
+      // const shouldCheckRateLimitAgain = await logRateLimitInformation(logger, octokit);
 
-      if (shouldCheckRateLimitAgain) {
-        setInterval(() => {
-          void logRateLimitInformation(logger, octokit);
-        }, 30_000);
-      }
+      // if (shouldCheckRateLimitAgain) {
+      //   setInterval(() => {
+      //     void logRateLimitInformation(logger, octokit);
+      //   }, 30_000);
+      // }
 
       const { isGitHubEnterpriseServer, gitHubEnterpriseServerVersion } =
         await getGitHubProductInformation(octokit);
--- src/utils.ts
+++ src/utils.ts
@@ -33,33 +33,33 @@ export const checkForUpdates = async (
   }
 };
 
-export const logRateLimitInformation = async (
-  logger: Logger,
-  octokit: Octokit,
-): Promise<boolean> => {
-  try {
-    const graphqlRateLimitResponse = (await octokit.graphql(
-      'query { rateLimit { limit remaining resetAt } }',
-    )) as { rateLimit?: { limit: number; remaining: number; resetAt: string } };
-    if (graphqlRateLimitResponse.rateLimit) {
-      const graphqlUsedRateLimit =
-        graphqlRateLimitResponse.rateLimit.limit -
-        graphqlRateLimitResponse.rateLimit.remaining;
+// export const logRateLimitInformation = async (
+//   logger: Logger,
+//   octokit: Octokit,
+// ): Promise<boolean> => {
+//   try {
+//     const graphqlRateLimitResponse = (await octokit.graphql(
+//       'query { rateLimit { limit remaining resetAt } }',
+//     )) as { rateLimit?: { limit: number; remaining: number; resetAt: string } };
+//     if (graphqlRateLimitResponse.rateLimit) {
+//       const graphqlUsedRateLimit =
+//         graphqlRateLimitResponse.rateLimit.limit -
+//         graphqlRateLimitResponse.rateLimit.remaining;
 
-      logger.info(
-        `GitHub GraphQL rate limit: ${graphqlUsedRateLimit}/${graphqlRateLimitResponse.rateLimit.limit} used - resets at ${graphqlRateLimitResponse.rateLimit.resetAt}`,
-      );
+//       logger.info(
+//         `GitHub GraphQL rate limit: ${graphqlUsedRateLimit}/${graphqlRateLimitResponse.rateLimit.limit} used - resets at ${graphqlRateLimitResponse.rateLimit.resetAt}`,
+//       );
 
-      return true;
-    } else {
-      logger.info(`GitHub rate limit is disabled.`);
-      return false;
-    }
-  } catch (e) {
-    logger.error(`Error checking GitHub rate limit: ${presentError(e)}`);
-    return true;
-  }
-};
+//       return true;
+//     } else {
+//       logger.info(`GitHub rate limit is disabled.`);
+//       return false;
+//     }
+//   } catch (e) {
+//     logger.error(`Error checking GitHub rate limit: ${presentError(e)}`);
+//     return true;
+//   }
+// };
 
 export const presentError = (e: unknown): string => {
   if (typeof e === 'string') return e;
